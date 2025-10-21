c179f76eb87b991eb06457f494d199e91aab3ca9
Ryan <rhdpham@gmail.com>
2025-10-20 22:16:13 -0400

    Initial Commit

---
+++ MovieRanker/MovieRankerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieRankerApp.swift
+//  MovieRanker
+//
+//  Created by Ryan Pham on 10/20/25.
+//
+
+import SwiftUI
+
+@main
+struct MovieRankerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
